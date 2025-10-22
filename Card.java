afb6c2c6634846cb533a116e18a070ef66216445
A5tral1s <matthew.sanii1@gmail.com>
2025-10-22 11:23:16 -0400

    Created basis for Card.java

---
+++ Card.java
@@ -0,0 +1,47 @@
+public class Card {
+    public enum colortype{
+        RED,
+        BLUE,
+        GREEN,
+        YELLOW,
+        ALL
+    }
+
+    public enum cardtype{
+        NUMBER,
+        SKIP,
+        ONE,
+        REVERSE,
+        WILD,
+        WILDTWO
+    }
+
+    public colortype col;
+    public cardtype type;
+    public int rank;
+
+    public Card(int color, int type, int rank){
+        if(type == 5 || type == 6){
+            this.type = cardtype.values()[type];
+            this.col = colortype.values()[4];
+        }
+        else{
+            this.type = cardtype.values()[type];
+            this.col = colortype.values()[color];
+            this.rank = rank;
+        }
+    }
+
+    public colortype getColor() {
+        return this.col;
+    }
+
+    public cardtype getType(){
+        return this.type;
+    }
+
+    public int getRank(){
+        return this.rank;
+    }
+
+}
