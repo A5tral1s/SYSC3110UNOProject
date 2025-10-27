ec1f1550dc40e81ecc86fceabb6f0ddcaec07174
A5tral1s <matthew.sanii1@gmail.com>
2025-10-26 20:13:27 -0400

    Updated Card.java
    Card.java has been updated with more detailed Javadoc comments

--- Card.java
+++ Card.java
@@ -1,3 +1,16 @@
+/**
+ * Class Card - one of the Cards in the UnoFlip Deck.
+ *
+ * This class is part of the UnoFlip game application.
+ *
+ * A "Card" represents a single card in the game. The game keeps
+ * track of a total of 116 as per the guidelines, each with a color,
+ * type, and rank.
+ *
+ * @author Matthew Sanii
+ * @version 1
+ */
+
 public class Card {
     public enum colortype{ //The four main card colors, plus ALL to represent the WILD cards
         RED,
@@ -21,6 +34,12 @@ public class Card {
     private cardtype type;
     private int rank;
 
+    /**
+    * Create a Card with a color, type, and rank.
+    * @param color The Color of the card
+    * @param type The type of card it is (wild, draw, skip)
+    * @param rank The numerical rank of the card.
+    */
     public Card(int color, int type, int rank){
         if(type == 5 || type == 6){ //If card type is one of the two Wild cards, set 'color' to ALL
             this.type = cardtype.values()[type];
@@ -33,18 +52,34 @@ public class Card {
         }
     }
 
+    /**
+    * Get the card's color
+    * @return The card's color
+    */
     public colortype getColor() {
         return this.col;
     }
 
+    /**
+    * Get the card's type
+    * @return The card's type
+    */
     public cardtype getType(){
         return this.type;
     }
 
+    /**
+    * Get the card's rank
+    * @return The card's rank
+    */
     public int getRank(){
         return this.rank;
     }
 
+    /**
+    * Return a description of the card, stating color, type, and rank where applicable.
+    * @return The description of the card as a String
+    */
     public String getDescription(){
         if(this.getRank()==Deck.NO_RANK){
             return this.getColor() +  " " + this.getType();
