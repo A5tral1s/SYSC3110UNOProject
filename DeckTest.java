0080038cf98d8fbe7f77f08ecdcfbf2388ed6746
A5tral1s <matthew.sanii1@gmail.com>
2025-10-27 18:47:14 -0400

    Update DeckTest.java
    Fixed shuffle test

--- DeckTest.java
+++ DeckTest.java
@@ -1,23 +1,34 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
 
 class DeckTest {
 
     @Test
     void shuffle() throws CloneNotSupportedException {
+        //Test to make sure that after a shuffle occurs (which happens upon making the deck)
         Deck deckOne = new Deck();
-        Deck deckTwo = new Deck();
-        assertNotEquals(deckOne.drawCard(), deckTwo.drawCard());
+        ArrayList<Card> firstcards = new ArrayList<>();
+        ArrayList<Card> secondcards = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            firstcards.add(deckOne.drawCard());
+        }
+        deckOne.reshuffleDiscards();
+        for(int j = 0; j < 10; j++){
+            secondcards.add(deckOne.drawCard());
+        }
+        assertNotEquals(firstcards, secondcards);
     }
 
     @Test
     void drawCard() {
+        //Test to make sure drawing cards works properly.
         Deck deck = new Deck();
         Card first = deck.drawCard();
         Card second = deck.drawCard();
         assertNotEquals(null, first);
-        if(first.getType() == second.getType() & first.getRank() == second.getRank()) {
+        if(first.getType() == second.getType() & first.getRank() == second.getRank() & first.getColor() == second.getColor()) {
             assertEquals(first.getDescription(), second.getDescription());
         } else{
             assertNotEquals(first.getDescription(), second.getDescription());
@@ -26,10 +37,12 @@ class DeckTest {
 
     @Test
     void topCard() {
+        //Test to make sure when discarding a card, it gets put in the discard pile properly.
         Deck deck = new Deck();
         assertNull(deck.topCard());
         Card drawn = deck.drawCard();
         deck.discard(drawn);
         assertEquals(drawn.getDescription(), deck.topCard().getDescription());
     }
+
 }
